59e48139b9fe2e21ea623292d214f00395f48014
pfdegorce <pfdegorce@outlook.com>
2022-12-12 14:23:48 +0100

    fonction log.sh qui fonctionne

--- log.sh
+++ log.sh
@@ -5,7 +5,7 @@ sudo mkdir -p /var/log/exercice/recent_logs
 
 #Base centrale de la boucle for
 
-for i in {1. .$1}
+for ((i=1; i<=$1; i++))
 do
 echo $(cat /dev/random |head -c 10) >> /var/log/exercice/recent_logs/$RANDOM
 done

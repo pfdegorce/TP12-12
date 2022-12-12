a672e2b3164bfc4cb528bdf6511b7fe6ba72882d
pfdegorce <pfdegorce@outlook.com>
2022-12-12 11:53:17 +0100

    premieres idess sur log

--- log.sh
+++ log.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+#Création du dossier dans lequel on va mettre les n fichiers
+mkdir - p /var/log/exercice/recent_logs
+
+#Base centrale de la boucle for
+echo $(cat /dev/random |head -c 10) >> /var/log/exercice/recent_logs/fichier_n
+

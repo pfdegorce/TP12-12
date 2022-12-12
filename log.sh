66e3195e381478287d74d8e14895bc5cd0a3dfb6
pfdegorce <pfdegorce@outlook.com>
2022-12-12 15:14:25 +0100

    version de log.sh avec mktemp

--- log.sh
+++ log.sh
@@ -3,10 +3,13 @@
 #Création du dossier dans lequel on va mettre les n fichiers
 sudo mkdir -p /var/log/exercice/recent_logs
 
-#Base centrale de la boucle for
+#Création des fichiers
 
 for ((i=1; i<=$1; i++))
 do
-echo $(cat /dev/random |head -c 10) >> /var/log/exercice/recent_logs/$RANDOM
+	#On récupère le chemin des fichiers tampons créés
+chemin=$(mktemp /var/log/exercice/recent_logs/XXXX.txt)
+	#On ajoute 10 caractères aléatoires dans ces fichiers
+echo $(cat /dev/random |head -c 10) >> $chemin
 done
 

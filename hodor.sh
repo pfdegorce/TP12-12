9721d3db256e81485a01cf9be9b982652167c3fa
pfdegorce <pfdegorce@outlook.com>
2022-12-13 10:13:24 +0100

    boucle if dans Hodor

--- hodor.sh
+++ hodor.sh
@@ -3,6 +3,15 @@
 inodes_libres=$(df -i / | tail -n 1 | cut -d ' ' -f 11)
 pourcentage_occupe=$(df -i / | tail -n 1 | cut -d ' ' -f 14 | sed 's/%//')
 
+echo "Le nombre d'inodes libres est :"
 echo $inodes_libres
+echo "Le pourcentage d'inode occupe est :"
 echo $pourcentage_occupe
+
+if ($pourcentage_occupe -gt 20)
+then 
+	echo "Il reste moins de 80% d'inodes disponibles"
+else
+	echo "Hodor survit"
+fi
 

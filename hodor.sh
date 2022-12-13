35a71b6445c32e4dc0153f2c40027230917884bf
pfdegorce <pfdegorce@outlook.com>
2022-12-13 09:41:49 +0100

    hodor renvoie les inodes libres et le pourcentage d'occupation

--- hodor.sh
+++ hodor.sh
@@ -1,1 +1,8 @@
-#!/bin/bash
+#!/bin/bas
+
+inodes_libres=$(df -i / | tail -n 1 | cut -d ' ' -f 11)
+pourcentage_occupe=$(df -i / | tail -n 1 | cut -d ' ' -f 14)
+
+echo $inodes_libres
+echo $pourcentage_occupe
+

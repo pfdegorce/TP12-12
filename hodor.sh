3fb35166a53dc8b11d72641e4f268c7feaf7af64
pfdegorce <pfdegorce@outlook.com>
2022-12-13 09:55:28 +0100

    hodor renvoie les inodes libres et le pourcentage d'occupation sans le pourcent

--- hodor.sh
+++ hodor.sh
@@ -1,7 +1,7 @@
 #!/bin/bas
 
 inodes_libres=$(df -i / | tail -n 1 | cut -d ' ' -f 11)
-pourcentage_occupe=$(df -i / | tail -n 1 | cut -d ' ' -f 14)
+pourcentage_occupe=$(df -i / | tail -n 1 | cut -d ' ' -f 14 | sed 's/%//')
 
 echo $inodes_libres
 echo $pourcentage_occupe
